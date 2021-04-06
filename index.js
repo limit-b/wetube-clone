13eacf6f1d0cb17c7d1e0c8e8f8c9e6006905957
limit-a <wjswogks152@gmail.com>
2021-04-06 14:31:26 +0900

    test_express-210406

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const app = express();
 
 const PORT = 4000;
 
+function handleHome(req, res) {
+  res.send('Hello from home\n');
+}
+
+function handleProfile(req, res) {
+  res.send('You are on my profile');
+}
+
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}/`);
 }
 
+app.get('/', handleHome);
+// app.get('/', (req, res) => {
+//   res.send('Hello from home\n');
+// });
+
+app.get('/profile', handleProfile);
+// app.get('/profile', (req, res) => {
+//   res.send('You are on my profile');
+// });
+
 app.listen(PORT, handleListening);
+// app.listen(PORT, () => {
+//   console.log(`Listening on: http://localhost:${PORT}/`);
+// });
