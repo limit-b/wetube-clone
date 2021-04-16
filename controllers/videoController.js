018d49c32d7ed2cf6a9b074c00559f07056f6ce9
limit-a <wjswogks152@gmail.com>
2021-04-16 18:11:07 +0900

    Add_search_join_login-210416

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,25 @@
 export const homeController = (req, res) =>
   res.render('home', { pageTitle: 'Home', potato: 1245 });
-export const searchController = (req, res) =>
-  res.render('search', { pageTitle: 'Search' });
+
+export const searchController = (req, res) => {
+  // 비구조화 할당(구조분해 할당)
+  const {
+    query: { term: searchingFor },
+  } = req;
+  // const searchingFor = req.query.term;
+  res.render('search', { pageTitle: 'Search', searchingFor });
+};
+
 // export const videosController = (req, res) => res.render('videos');
+
 export const uploadController = (req, res) =>
   res.render('upload', { pageTitle: 'Upload' });
+
 export const videoDetailController = (req, res) =>
   res.render('video-detail', { pageTitle: 'Video Detail' });
+
 export const editVideoController = (req, res) =>
   res.render('edit-video', { pageTitle: 'Edit Video' });
+
 export const deleteVideoController = (req, res) =>
   res.render('delete-video', { pageTitle: 'Delete Video' });
