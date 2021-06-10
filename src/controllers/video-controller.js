e9553dcc932dfd347d2e79ab16edc23140283627
limit-a <wjswogks152@gmail.com>
2021-06-10 22:14:23 +0900

    test_pug-210610

--- src/controllers/video-controller.js
+++ src/controllers/video-controller.js
@@ -1,17 +1,14 @@
-export const homeController = (req, res) => res.render('home');
+export const homeController = (req, res) =>
+  res.render('home', { pageTitle: 'Home', potato: 'tomato' });
 
 export const searchController = (req, res) => res.send('search page');
 
 export const uploadVideoController = (req, res) =>
   res.send('upload video page');
 
-export const watchVideoController = (req, res) => res.render('watch');
+export const watchVideoController = (req, res) => res.render('watch-video');
 
-export const editVideoController = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang="ko"><head><title>WeTube</title></head><body><h1>edit video page, #id : ${req.params.id}</h1><footer>&copy; 2021 WeTube - All rights reserved</footer></body></html>`
-  );
-};
+export const editVideoController = (req, res) => res.render('edit-video');
 
 export const deleteVideoController = (req, res) => {
   return res.send(`delete video page, #id : ${req.params.id}`);
