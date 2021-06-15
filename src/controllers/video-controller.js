34e5c5e6da26a01d9d143d90081809063013a7c8
limit-a <wjswogks152@gmail.com>
2021-06-15 22:55:27 +0900

    test_db_array-210615

--- src/controllers/video-controller.js
+++ src/controllers/video-controller.js
@@ -1,32 +1,32 @@
 const fakeUser = { username: 'limit', loggedIn: false };
+const videosDB = [
+  {
+    title: 'first video',
+    rating: 3,
+    comments: 6,
+    createdAt: `${8} minutes ago`,
+    views: 200,
+    id: 0,
+  },
+  {
+    title: 'video #2',
+    rating: 4,
+    comments: 4,
+    createdAt: `${6} minutes ago`,
+    views: 100,
+    id: 1,
+  },
+  {
+    title: 'qwerty',
+    rating: 5,
+    comments: 2,
+    createdAt: `${2} minutes ago`,
+    views: 59,
+    id: 2,
+  },
+];
 
 export const homeController = (req, res) => {
-  const videosDB = [
-    {
-      title: 'first video',
-      rating: 3,
-      comments: 6,
-      createdAt: `${8} minutes ago`,
-      views: 200,
-      id: 1,
-    },
-    {
-      title: 'video #2',
-      rating: 4,
-      comments: 4,
-      createdAt: `${6} minutes ago`,
-      views: 100,
-      id: 2,
-    },
-    {
-      title: 'qwerty',
-      rating: 5,
-      comments: 2,
-      createdAt: `${2} minutes ago`,
-      views: 59,
-      id: 3,
-    },
-  ];
   return res.render('home', {
     pageTitle: 'Home',
     potato: 'tomato',
@@ -40,7 +40,14 @@ export const searchController = (req, res) => res.send('search page');
 export const uploadVideoController = (req, res) =>
   res.send('upload video page');
 
-export const watchVideoController = (req, res) => res.render('watch-video');
+export const watchVideoController = (req, res) => {
+  const { id } = req.params;
+  const video = videosDB[id];
+  return res.render('watch-video', {
+    pageTitle: `Watching ${video.title}`,
+    video,
+  });
+};
 
 export const editVideoController = (req, res) => res.render('edit-video');
 
