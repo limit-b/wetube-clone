5b1059caeb25cffbd53f84557df522b0f2974c25
limit-a <wjswogks152@gmail.com>
2021-06-24 18:58:38 +0900

    Test(database): Add mongoose function for CRUD

--- src/controllers/video-controller.js
+++ src/controllers/video-controller.js
@@ -1,38 +1,14 @@
+import VideoModel from '../models/Video';
+
 const fakeUser = { username: 'limit', loggedIn: false };
-let videosDB = [
-  {
-    title: 'first video',
-    rating: 3,
-    comments: 6,
-    createdAt: `${8} minutes ago`,
-    views: 200,
-    id: 0,
-  },
-  {
-    title: 'video #2',
-    rating: 4,
-    comments: 4,
-    createdAt: `${6} minutes ago`,
-    views: 100,
-    id: 1,
-  },
-  {
-    title: 'qwerty',
-    rating: 5,
-    comments: 2,
-    createdAt: `${2} minutes ago`,
-    views: 59,
-    id: 2,
-  },
-];
 
 export const homeController = (req, res) => {
-  return res.render('home', {
-    pageTitle: 'Home',
-    potato: 'tomato',
-    fakeUser,
-    videosDB,
+  console.log('start');
+  VideoModel.find({}, (error, videosDB) => {
+    console.log(error, videosDB);
+    return res.render('home', { pageTitle: 'Home', videosDB, fakeUser });
   });
+  console.log('finish');
 };
 
 export const searchController = (req, res) => res.send('search page');
@@ -43,33 +19,20 @@ export const getUploadVideoController = (req, res) => {
 
 export const postUploadVideoController = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: '0 minutes ago',
-    views: 0,
-    id: videosDB.length,
-  };
-  videosDB.push(newVideo);
   return res.redirect('/');
 };
 
 export const watchVideoController = (req, res) => {
   const { id } = req.params;
-  const video = videosDB[id];
   return res.render('watch-video', {
-    pageTitle: `Watching: ${video.title}`,
-    video,
+    pageTitle: `Watching: `,
   });
 };
 
 export const getEditVideoController = (req, res) => {
   const { id } = req.params;
-  const video = videosDB[id];
   return res.render('edit-video', {
-    pageTitle: `Editing: ${video.title}`,
-    video,
+    pageTitle: `Editing: `,
   });
 };
 
@@ -78,9 +41,6 @@ export const postEditVideoController = (req, res) => {
     params: { id },
     body: { title },
   } = req;
-  // temp code
-  videosDB[id].title = title;
-  //
   return res.redirect(`/videos/${id}`);
 };
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import './db';
-import './models/Video';
 import express from 'express';
 import morgan from 'morgan';
 import globalRouter from './routers/global-router';
@@ -9,10 +7,7 @@ import videosRouter from './routers/video-router';
 const app = express();
 const logger = morgan('dev');
 
-const PORT = 4000;
-
 app.set('view engine', 'pug');
-// cwd ---> current working directory
 app.set('views', `${process.cwd()}/src/views`);
 // "body-parser" deplicated
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +16,4 @@ app.use('/', globalRouter);
 app.use('/users', usersRouter);
 app.use('/videos', videosRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on: http://localhost:${PORT}/`);
-
-app.listen(PORT, handleListening);
+export default app;
