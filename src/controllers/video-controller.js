1f2725135db888f743f07ff304ad57e1d3b05bff
limit-a <wjswogks152@gmail.com>
2021-07-09 22:42:22 +0900

    Feat(router): Change to hex-regex for watch page

--- src/controllers/video-controller.js
+++ src/controllers/video-controller.js
@@ -3,62 +3,64 @@ import VideoModel from '../models/Video';
 const fakeUser = { username: 'limit', loggedIn: false };
 
 export const homeController = async (req, res) => {
-  try {
-    const videosDB = await VideoModel.find({});
-    console.log(videosDB);
-    return res.render('home', { pageTitle: 'Home', videosDB, fakeUser });
-  } catch (error) {
-    return res.render('server-error', { error });
-  }
+    try {
+        const videosDB = await VideoModel.find({});
+        console.log(videosDB);
+        return res.render('home', { pageTitle: 'Home', videosDB, fakeUser });
+    } catch (error) {
+        return res.render('server-error', { error });
+    }
 };
 
 export const searchController = (req, res) => res.send('search page');
 
 export const getUploadVideoController = (req, res) => {
-  return res.render('upload-video', { pageTitle: 'Upload Video' });
+    return res.render('upload-video', { pageTitle: 'Upload Video' });
 };
 
 export const postUploadVideoController = async (req, res) => {
-  const { title, description, hashtags } = req.body;
-  try {
-    await VideoModel.create({
-      title,
-      description,
-      // createdAt: Date.now(),
-      hashtags: hashtags.split(',').map((word) => `#${word}`),
-      // meta: { views: 0, rating: 0 },
-    });
-    return res.redirect('/');
-  } catch (error) {
-    return res.render('upload-video', {
-      pageTitle: 'Upload Video',
-      errorMessage: error._message,
-    });
-  }
+    const { title, description, hashtags } = req.body;
+    try {
+        await VideoModel.create({
+            title,
+            description,
+            // createdAt: Date.now(),
+            hashtags: hashtags.split(',').map((word) => `#${word}`),
+            // meta: { views: 0, rating: 0 },
+        });
+        return res.redirect('/');
+    } catch (error) {
+        return res.render('upload-video', {
+            pageTitle: 'Upload Video',
+            errorMessage: error._message,
+        });
+    }
 };
 
-export const watchVideoController = (req, res) => {
-  const { id } = req.params;
-  return res.render('watch-video', {
-    pageTitle: `Watching: `,
-  });
+export const watchVideoController = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const video = await VideoModel.findById(id);
+        return res.render('watch-video', { pageTitle: video.title, video });
+    } catch (error) {
+        console.log(error);
+        return res.redirect('/');
+    }
 };
 
 export const getEditVideoController = (req, res) => {
-  const { id } = req.params;
-  return res.render('edit-video', {
-    pageTitle: `Editing: `,
-  });
+    const { id } = req.params;
+    return res.render('edit-video', { pageTitle: `Editing: ` });
 };
 
 export const postEditVideoController = (req, res) => {
-  const {
-    params: { id },
-    body: { title },
-  } = req;
-  return res.redirect(`/videos/${id}`);
+    const {
+        params: { id },
+        body: { title },
+    } = req;
+    return res.redirect(`/videos/${id}`);
 };
 
 export const deleteVideoController = (req, res) => {
-  return res.send(`delete video page, #id : ${req.params.id}`);
+    return res.send(`delete video page, #id : ${req.params.id}`);
 };
--- src/routers/video-router.js
+++ src/routers/video-router.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import {
-  getUploadVideoController,
-  postUploadVideoController,
-  watchVideoController,
-  getEditVideoController,
-  postEditVideoController,
-  deleteVideoController,
+    getUploadVideoController,
+    postUploadVideoController,
+    watchVideoController,
+    getEditVideoController,
+    postEditVideoController,
+    deleteVideoController,
 } from '../controllers/video-controller';
 
 const videosRouter = express.Router();
 
 videosRouter
-  .route('/upload')
-  .get(getUploadVideoController)
-  .post(postUploadVideoController);
-videosRouter.get('/:id(\\d+)', watchVideoController);
+    .route('/upload')
+    .get(getUploadVideoController)
+    .post(postUploadVideoController);
+videosRouter.get('/:id([0-9a-f]{24})', watchVideoController);
 videosRouter
-  .route('/:id(\\d+)/edit')
-  .get(getEditVideoController)
-  .post(postEditVideoController);
-videosRouter.get('/:id(\\d+)/delete', deleteVideoController);
+    .route('/:id([0-9a-f]{24})/edit')
+    .get(getEditVideoController)
+    .post(postEditVideoController);
+videosRouter.get('/:id([0-9a-f]{24})/delete', deleteVideoController);
 
 export default videosRouter;
