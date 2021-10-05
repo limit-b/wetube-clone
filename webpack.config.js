c3d059e868a4b57ad1991ce35b0b560f6f09676a
limit-a <wjswogks152@gmail.com>
2021-10-06 00:05:27 +0900

    Chore(json): Change setting of webpack and nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,16 @@
 const path = require('path');
-// const autoprefixer = require('autoprefixer');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // const MODE = process.env.WEBPACK_ENV;
-// const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js');
-// const OUTPUT_DIR = path.join(__dirname, 'static');
 
 console.log(path.resolve(__dirname, 'assets', 'js'));
 
 const config = {
-    // entry: ['@babel/polyfill', ENTRY_FILE],
     entry: './src/client/js/main.js',
-    // output: { path: OUTPUT_DIR, filename: '[name].js' },
     output: {
-        path: path.resolve(__dirname, 'assets', 'js'),
-        filename: 'main.js',
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'js/main.js',
+        clean: true,
     },
     module: {
         rules: [
@@ -33,32 +29,17 @@ const config = {
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader',
-                    // { loader: 'style-loader' },
-                    // { loader: MiniCssExtractPlugin.loader },
-                    // { loader: 'css-loader' },
-                    // {
-                    //     loader: 'postcss-loader',
-                    //     options: {
-                    //         postcssOptions: {
-                    //             plugins: [
-                    //                 [
-                    //                     'autoprefixer',
-                    //                     { overrideBrowserslist: 'cover 99.5%' },
-                    //                 ],
-                    //             ],
-                    //         },
-                    //     },
-                    // },
-                    // { loader: 'sass-loader' },
                 ],
             },
         ],
     },
-    // plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
+    plugins: [new MiniCssExtractPlugin({ filename: 'css/styles.css' })],
     mode: 'development',
+    watch: true,
 };
 
 module.exports = config;
